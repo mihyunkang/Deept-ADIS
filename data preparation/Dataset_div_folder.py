c2a91fc9e0a1e36c2e6c121d13386913658580a9
hyeon-jeong <baekhyeonjeong0326@gmail.com>
2020-11-02 22:14:26 +0900

    2020.11.02
    데이터셋 다시 분할

--- data preparation/Dataset_div_folder.py
+++ data preparation/Dataset_div_folder.py
@@ -2,38 +2,37 @@
 #fake/real 데이터 각각!!
 import os
 import shutil
+import random
 
 ##### 파일 불러오기
 #real 데이터
-path_real = "D:/deeper_forensics/real"
+path_real = "D:/frame_dataset/real/"
 #fake 데이터
-path_fake = "D:/deeper_forensics/fake" #path_fake = "D:/full_data/fake"
+path_fake = "D:/frame_dataset/fake/" #path_fake = "D:/full_data/fake"
 #파일경로로 불러옴
 real_file = os.listdir(path_real)
 fake_file = os.listdir(path_fake)
+random.shuffle(real_file)
+random.shuffle(fake_file)
 
 ##### 옮기는 경로~~~
 #test 데이터
-dest_test = "D:/deeper_forensics/test/"
+dest_test = "D:/dataset/test/"
 #train 데이터
-dest_train = "D:/deeper_forensics/train/"
+dest_train = "D:/dataset/train/"
 #val 데이터
-dest_val = "D:/deeper_forensics/val/"
+dest_val = "D:/dataset/val/"
 
-#real file - 7:2:1 분할
+#real file - 2:8 분할
 for i in range(len(real_file)):
-    if(i<len(real_file)*0.7): #train-real
-        shutil.move(path_real+'/'+real_file[i], dest_train+'real/'+real_file[i])
-    elif(len(real_file)*0.7<i and i <=len(real_file)*0.9): #test-real
-        shutil.move(path_real+'/'+real_file[i], dest_test+'real/'+real_file[i])
+    if(i<len(real_file)*0.2): #train-real
+        shutil.copyfile(path_real+real_file[i], dest_train+'real/'+real_file[i])
     else: #val-real
-        shutil.move(path_real+'/'+real_file[i], dest_val+'real/'+real_file[i])
+        shutil.copyfile(path_real+real_file[i], dest_test+'real/'+real_file[i])
 
-#fake file - 7:2:1 분할
+#fake file - 2:8 분할
 for i in range(len(fake_file)):
-    if(i<len(fake_file)*0.7): #train-real
-        shutil.move(path_fake+'/'+fake_file[i], dest_train+'fake/'+fake_file[i])
-    elif(len(fake_file)*0.7<i and i <=len(fake_file)*0.9): #test-real
-        shutil.move(path_fake+'/'+fake_file[i], dest_test+'fake/'+fake_file[i])
+    if(i<len(fake_file)*0.2): #train-real
+        shutil.copyfile(path_fake+fake_file[i], dest_train+'fake/'+fake_file[i])
     else: #val-real
-        shutil.move(path_fake+'/'+fake_file[i], dest_val+'fake/'+fake_file[i])
+        shutil.copyfile(path_fake+fake_file[i], dest_test+'fake/'+fake_file[i])
